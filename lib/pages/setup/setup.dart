0f161729623a7ada217465cd5500268d45e02b4d
F1rman <ihorkharchyshyn@gmail.com>
2021-04-03 15:17:58 +0300

    setup slider

--- lib/pages/setup/setup.dart
+++ lib/pages/setup/setup.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'widgets/HeightSelect.dart';
-import 'widgets/YearsSelect.dart';
-import 'widgets/weightSelect.dart';
-import 'widgets/GenderSelect.dart';
-import 'widgets/Stepper.dart';
-
+import 'package:carousel_slider/carousel_slider.dart';
 
 class SetupRoute extends StatefulWidget {
   SetupRoute({this.user, this.cb});
@@ -33,7 +28,7 @@ class SetupState extends State<SetupRoute> {
   @override
 
   Widget build(BuildContext context) {
-    
+    Size size = MediaQuery.of(context).size;
     void changeIndex(){
       setState(() {
         if(_index>2){
@@ -47,26 +42,35 @@ class SetupState extends State<SetupRoute> {
             });
     }
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          child: Column(
-            children: [
-              NavStepper(index: _index),
-              Expanded(
-                child: IndexedStack(
-                  index: _index,
-                  children: [
-                    GenderChoose(),
-                    WeightChoose(),
-                    HeightChoose(),
-                    YearsChoose(),
-                  ],
-                ),
-              ),
-            ],
+      body: CarouselSlider(
+  options: CarouselOptions(
+    height: size.height,
+      aspectRatio: 16/9,
+      viewportFraction: 1,
+      initialPage: 1,
+      enableInfiniteScroll: false,
+      reverse: false,
+      autoPlay: false,
+      autoPlayCurve: Curves.fastOutSlowIn,
+      // onPageChanged: ,
+      scrollDirection: Axis.horizontal,
+    ),
+  items: [1,2,3,4,5].map((i) {
+    return Builder(
+      builder: (BuildContext context) {
+        return Container(
+          height: double.infinity,
+          width: MediaQuery.of(context).size.width,
+          margin: EdgeInsets.symmetric(horizontal: 0.0),
+          decoration: BoxDecoration(
+            color: Colors.amber
           ),
-        ),
-      ),
+          child: Text('text $i', style: TextStyle(fontSize: 16.0),)
+        );
+      },
+    );
+  }).toList(),
+),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
          changeIndex();
