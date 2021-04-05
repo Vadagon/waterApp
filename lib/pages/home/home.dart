be17273dcb35a644ae4d2ee31807211652db00d6
F1rman <ihorkharchyshyn@gmail.com>
2021-04-05 20:44:35 +0300

    Home page 30%

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeRoute extends StatefulWidget {
   HomeRoute({this.user, this.cb});
@@ -22,27 +23,141 @@ class HomeState extends State<HomeRoute> {
 
   @override
   Widget build(BuildContext context) {
+    Size contextSize = MediaQuery.of(context).size;
+    EdgeInsets padding = MediaQuery.of(context).padding;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('2nd screen'),
-      ),
-      body: Center(
+      body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: [
+            Container(
+              width: contextSize.width,
+              height: contextSize.height - padding.top - padding.bottom,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomRight,
+                  end: Alignment.topLeft,
+                  stops: [
+                    0.0,
+                    0.8,
+                  ],
+                  colors: [
+                    Color(0xff00D7CA),
+                    Color(0xcff1B61CB),
+                  ],
+                ),
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: SvgPicture.asset(
+                            'assets/img/bulb.svg',
+                          ),
+                        ),
+                        IconButton(
+                          icon: const Icon(
+                            Icons.settings,
+                            color: Colors.white,
+                            size: 28,
+                          ),
+                          tooltip: 'Settings',
+                          onPressed: () {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text('This is a snackbar')));
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 15, horizontal: 45),
+                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 5),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                            color: const Color(0x40000000),
+                            offset: Offset(4, 4),
+                            blurRadius: 4,
+                            spreadRadius: 0)
+                      ],
+                      color: Color(0xA668C4FB),
+                    ),
+                    child: Text(
+                      'Коли організм зневоднений, обмін речовин сповільнюється. Порушення метаболізму може привести до набору ваги.',
+                      style: Theme.of(context).textTheme.bodyText2,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        clipBehavior: Clip.antiAlias,
+                        margin:
+                            EdgeInsets.symmetric(vertical: 15, horizontal: 45),
+                        width: 17,
+                        height: contextSize.height / 5,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          boxShadow: [
+                            BoxShadow(
+                                color: const Color(0x40000000),
+                                offset: Offset(4, 4),
+                                blurRadius: 4,
+                                spreadRadius: 0)
+                          ],
+                          color: Color(0xFFFFFFFF),
+                        ),
+                        child: Stack(
+                          children: [
+                            Positioned(
+                              bottom: 0,
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: 1,
+                                    width: 17,
+                                    // color: Color(0xFFFFFFFF),
+                                    color: Colors.red,
+                                  ),
+                                  Container(
+                                    height: 200,
+                                    width: 17,
+                                    color: Color(0xA668C4FB),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    child:Column(
+                      children: [
+                         SvgPicture.asset(
+                            'assets/img/aquarium1.svg',
+                            width: contextSize.height / 5,
+                          ),
+                          Text('0/1900',
+                           style: Theme.of(context).textTheme.bodyText1,
+                      textAlign: TextAlign.center,),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pushNamed(context, "/");
-        },
-        tooltip: 'Prev',
-        child: Icon(Icons.skip_previous),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/themeData.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+ThemeData appTheme = ThemeData(
+        primarySwatch: Colors.lightBlue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: 'Montserrat',
+         textTheme: TextTheme(
+      headline1: TextStyle(fontSize: 26.0, fontWeight: FontWeight.bold, color: Color(0xffffffff)),
+      headline6: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold,),
+      bodyText1: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold,color: Colors.white),
+      bodyText2: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold,),
+      caption: TextStyle(fontSize: 24.0, color: Color(0xff222222), fontWeight: FontWeight.bold,),
+    ),);
