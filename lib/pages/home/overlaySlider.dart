592ec69b7359b3518aa9676f9da45d2ebed053bb
F1rman <ihorkharchyshyn@gmail.com>
2021-04-11 13:21:34 +0300

    overlay ok

--- lib/pages/home/overlaySlider.dart
+++ lib/pages/home/overlaySlider.dart
@@ -66,88 +66,127 @@ class SliderOverlay extends StatefulWidget {
 class _SliderOverlayState extends State<SliderOverlay> {
   _SliderOverlayState(this.cb);
   final Function cb;
-  double ml=0;
+  double ml = 1000;
+  double btnPos = 0;
+  double textPos = 0;
+  double dragPoint = 0;
+
+  
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Row(
-      children: [
-        GestureDetector(
-          child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(5)),
-                color: Color(0xFF9DEDFF),
-              ),
-              height: widget.contextSize.height / 2,
-              width: 50,
-              child: Stack(
-                children: [
-                  Positioned(
-                    right: 0,
-                    top: dragPoint,
-                    child: Container(
-                      color: Color(0xFFFFFFFF),
-                      width: 20,
-                      height: 2,
-                    ),
-                  ),
-                ],
-              )),
-          onVerticalDragUpdate: (axis) {
-            setState(() {
-              dragPoint = axis.localPosition.dy;
-            });
-            var maxHeight = widget.contextSize.height / 2;
-            var percent = (dragPoint / maxHeight )* 100;
-            var maxLitres = 1000;
-            var litres = maxLitres * (percent/100);
-             ml = (litres - 1000).abs();
-            print(ml);
-            // print(axis.localPosition.direction);
-            // print(axis.localPosition.distance);
-            // print(axis.localPosition.distanceSquared);
-          },
-        ),
-        Container(
-          width: 200,
-          height: widget.contextSize.height / 2 + 20,
-          child: Stack(
+    return GestureDetector(
+      child: Container(
+          height: widget.contextSize.height / 2,
+          child: Row(
             children: [
-              Positioned(
-                top: dragPoint,
-                child: Container(
-                  width: 200,
-                  height: 100,
-                  // color: Colors.red,
-                  child: Text('+ ${ml.round()}',
-                      style: Theme.of(context).textTheme.bodyText1),
-                ),
-              ),
-              Positioned(
-                top: dragPoint + 40,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                    color: Color(0xFF27C1E2),
-                  ),
-                  width: 80,
-                  height: 40,
-                  child: Center(
-                    child: TextButton(
-                      child: Text('add',
-                          style: Theme.of(context).textTheme.bodyText1),
-                      onPressed: () {
-                        cb();
-                        print((int.parse(dragPoint.toStringAsFixed(0))*100) );
-                      },
+                Container(
+                            clipBehavior: Clip.antiAlias,
+
+                    decoration: BoxDecoration(
+                      
+                      borderRadius: BorderRadius.all(Radius.circular(5)),
+                      color: Color(0xFF9DEDFF),
                     ),
-                  ),
+                    width: 50,
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          right: 0,
+                          top: dragPoint,
+                          child: Container(
+                            color: Color(0xFFFFFFFF),
+                            width: 20,
+                            height: 2,
+                          ),
+                        ),
+                      ],
+                    ),),
+                
+              Container(
+                width: 200,
+                height: widget.contextSize.height / 2 + 20,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: dragPoint - textPos,
+                      left: 10,
+                      child: Container(
+                        width: 200,
+                        height: 100,
+                        // color: Colors.red,
+                        child: Text('+ ${((ml/25).floor())*25} ml',
+                            style: Theme.of(context).textTheme.bodyText1),
+                      ),
+                    ),
+                    Positioned(
+                      top: dragPoint + 40 - btnPos,
+                      left: 10,
+
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          color: Color(0xFF27C1E2),
+                        ),
+                        width: 80,
+                        height: 40,
+                        child: Center(
+                          child: TextButton(
+                            child: Text('add',
+                                style: Theme.of(context).textTheme.bodyText1),
+                            onPressed: () {
+                              cb();
+                              print(((ml/25).floor())*25);
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
+              )
             ],
-          ),
-        )
-      ],
-    ));
+          ),),
+          onVerticalDragUpdate: (axis) {
+                setState(() {
+                  dragPoint = axis.localPosition.dy;
+                  var maxHeight = widget.contextSize.height / 2;
+                  var percent = (dragPoint / maxHeight) * 100;
+                  var maxLitres = 1000;
+                  var litres = maxLitres * (percent / 100);
+                  ml = (litres - 1000).abs();
+                  if (ml > 1000) {
+                    ml = 1000;
+                  } 
+                   if (ml < 200) {
+                     textPos = 80;
+                     btnPos = 80;
+                    
+                  } 
+                  else {
+                    textPos = 0;
+                     btnPos = 0;
+                    
+                  }
+                  if (dragPoint > maxHeight) {
+                    ml = 0;
+                    dragPoint = maxHeight ;
+                  } 
+                   if (dragPoint < 0) {
+                    ml = 1000;
+                    dragPoint = 0;
+                  } 
+                  else {
+                  //  dragPoint =  ((((dragPoint/10).floor())*10)).toDouble();
+                  }
+                    print(dragPoint);
+                // print(ml);
+                });
+
+              
+                // print(axis.localPosition.direction);
+                // print(axis.localPosition.distance);
+                // print(axis.localPosition.distanceSquared);
+              },
+    );
   }
 }
